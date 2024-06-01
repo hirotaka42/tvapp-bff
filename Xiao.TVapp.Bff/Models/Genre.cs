7b6479ea79cb55409589380cf3c0d5dbb4d245f5
hirotaka42 <kodama.hirotaka.190380@roivy.net>
2024-06-01 16:17:20 +0900

    feat: カテゴリー別ランキング取得のエンドポイントを追加

---
+++ Xiao.TVapp.Bff/Models/Genre.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Xiao.TVapp.Bff.Models
+{
+    public enum Genre
+    {
+        drama,
+        variety,
+        anime,
+        news_documentary,
+        sports,
+        other
+    }
+}
+

00f81706b70d78c64cc7f72bcf9461f7f40ffc34
hirotaka42 <kodama.hirotaka.190380@roivy.net>
2024-06-12 19:29:20 +0900

    feat: CORSポリシー設定で特定のオリジン（localhost）を許可し、ポート番号に関わらず許可するように設定

--- Xiao.TVapp.Bff/Program.cs
+++ Xiao.TVapp.Bff/Program.cs
@@ -6,7 +6,7 @@ builder.Services.AddCors(options =>
     options.AddDefaultPolicy(
         policy =>
         {
-            policy.WithOrigins("http://localhost:3000")
+            policy.SetIsOriginAllowed(origin => new Uri(origin).Host == "localhost")
                 .AllowAnyHeader()
                 .AllowAnyMethod();
         });

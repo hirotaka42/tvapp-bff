48c9472e98166cba9cf14449f4395b6fa00fde6e
hirotaka42 <kodama.hirotaka.190380@roivy.net>
2024-06-21 01:04:00 +0900

    update: DockerでSQliteを使用したDBが作成できなかった問題を修正

--- Xiao.TVapp.Bff/Contexts/StreamingUrlsContext.cs
+++ Xiao.TVapp.Bff/Contexts/StreamingUrlsContext.cs
@@ -9,6 +9,7 @@ namespace Xiao.TVapp.Bff.Contexts
         public StreamingUrlsContext(DbContextOptions<StreamingUrlsContext> options)
             : base(options)
         {
+            Database.EnsureCreated(); // Create the database if it doesn't exist
         }
 
         public DbSet<StreamingUrls> StreamingUrls { get; set; }
